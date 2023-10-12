04d5d3fbc585e3bbb6cf018f7fc875e25853faf8
marisanity <marion.karlsson@gmail.com>
2023-10-12 13:54:41 +0300

    add bs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
 function toggleMenu(event) {
-    var menuBtn = document.getElementById('menuBtn');
-    var menuView = document.getElementById('menuView');
-  
-    // If a link is clicked or the menu is open, close the menu
-    if (event.target.tagName === 'A' || menuBtn.classList.contains('close')) {
-      menuBtn.classList.remove('close');
-      menuView.style.display = 'none';
-    } else {
-      menuBtn.classList.add('close');
-      menuView.style.display = 'flex';
-    }
+  var menuBtn = document.getElementById('menuBtn');
+  var menuView = document.getElementById('menuView');
+
+  // If a link is clicked or the menu is open, close the menu
+  if (event.target.tagName === 'A' || menuBtn.classList.contains('close')) {
+    menuBtn.classList.remove('close');
+    menuView.style.display = 'none';
+  } else {
+    menuBtn.classList.add('close');
+    menuView.style.display = 'flex';
   }
+}
