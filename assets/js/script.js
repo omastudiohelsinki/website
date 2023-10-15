65c99657cc4ca3a6f5bde4cc87c608cef7f55b5c
marisanity <marion.karlsson@gmail.com>
2023-10-15 16:01:54 +0300

    minor improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,27 @@ function toggleMenu(event) {
   var menuBtn = document.getElementById('menuBtn');
   var menuView = document.getElementById('menuView');
 
-  // If a link is clicked or the menu is open, close the menu
-  if (event.target.tagName === 'A' || menuBtn.classList.contains('close')) {
+  if (event.target.tagName === 'A' || event.target.tagName === 'IMG' || menuBtn.classList.contains('close')) {
     menuBtn.classList.remove('close');
     menuView.style.display = 'none';
   } else {
     menuBtn.classList.add('close');
     menuView.style.display = 'flex';
   }
+}
+
+let scrollTopBtn = document.getElementById("scrollTopButton");
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
